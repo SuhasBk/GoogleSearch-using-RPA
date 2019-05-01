a4732528337a6241b6af408fb4ff292027e89273
Suhas B Kowligi <kowligi1998@gmail.com>
2019-05-01 22:17:10 +0530

    Update yt.py

--- yt.py
+++ yt.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-import platform
-import re
-from subprocess import *
-import webbrowser
+import re,os,sys
+from subprocess import PIPE,run
 import requests
-from bs4 import *
-import os
+from bs4 import BeautifulSoup
 
-search=input("Enter the search term\n").split()
+if len(sys.argv[1:])==0:
+    exit("Usage : yt.py [search_term]")
+else:
+    search_term = ' '.join(sys.argv[1:])
+
+headers = {'user-agent':'inspark'}
+
 try:
-    r = requests.get("http://youtube.com/results?search_query=" + '+'.join(search),headers={'user-agent':'WTF'})
+    r = requests.get("http://youtube.com/results?search_query=" + '+'.join(search_term),headers=headers)
 except requests.HTTPError as err:
     print(err)
     exit()
+
 print("Searching....")
 s = BeautifulSoup(r.text, 'html.parser')
 l = s.select('div .yt-lockup-content')
@@ -21,25 +25,32 @@ urls = []
 titles = []
 views = []
 uploaded = []
+uploaders = []
 durations =[]
 
 for i in l:
-    meta = i.findAll('li')[:2]
-    uploaded.append(meta[0].text)
-    views.append(meta[1].text)
-    durations.append(i.find('span',attrs={'class':'accessible-description'}).text)
-    urls.append('http://youtube.com'+i.find('a').get('href'))
-    titles.append(i.find('a').get('title'))
-
-
-for i,t,up,d,v,ur in zip(range(3),titles,uploaded,durations,views,urls):
     try:
-        p=requests.get(ur)
+        meta = i.findAll('li')[:2]
+        uploaded.append(meta[0].text)
+        views.append(meta[1].text)
+        durations.append(i.find('span',attrs={'class':'accessible-description'}).text)
+        urls.append('http://youtube.com'+i.find('a').get('href'))
+        titles.append(i.find('a').get('title'))
+        uploader = i.findAll('a')[1].get('href')
+        uploaders.append(re.findall('/user/(\w+)',uploader)[0])
+    except:
+        pass
+
+for i,t,up,d,v,ur,us in zip(range(5),titles,uploaded,durations,views,urls,uploaders):
+    try:
+        p=requests.get(ur,headers=headers)
         opinion=re.findall(r'(\d+(,\d+)*)? other people',p.text)
         likes = opinion[0][0]
         dislikes=opinion[2][0]
-        print((str(i)+' : '+t+'\nYouTube URL : '+ur+'\nUPLOADED : '+up+'\n'+d.upper().replace(' - ','')+'\nVIEWS : '+v+'\nLIKES : '+likes+'\nDISLIKES : '+dislikes+'\n'))
-    except:
+        print(str(i)+' : '+t+'\nYouTube URL : '+ur+'\nUPLOADED : '+up+'\n'+d.upper().replace(' - ','')+'\nVIEWS : '+v+'\nLIKES : '+likes+'\nDISLIKES : '+dislikes+'\nUPLOADER : '+us+'\n')
+    except requests.exceptions.ConnectionError:
+        pass
+    except IndexError:
         pass
 
 
@@ -50,7 +61,7 @@ while True:
             print(j)
             dorv=input("Ok! So, do you want to stream the video (type 'v') or download (type 'd', requires youtube-dl) it?\n")
             if dorv == 'd':
-                mp3=input("Cool! BTW, do you want me to download the audio (type 'a') or video (type 'v')?\n")
+                mp3=input("Do you want to download the audio (type 'a') or video (type 'v')?\n")
                 if mp3 == 'v':
                     print("Downloading video...")
                     run("youtube-dl "+j,shell=True)
