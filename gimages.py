1a13477332486ab27f0352058b481f6f225afd7a
Suhas B Kowligi <kowligi1998@gmail.com>
2019-07-11 16:13:38 +0530

    increased accuracy

--- gimages.py
+++ gimages.py
@@ -4,9 +4,9 @@ from bs4 import *
 from PIL import Image
 
 if len(sys.argv[1:])==0:
-    exit("Usage : gimages.py [search_term]")
-
-search_term = '_'.join(sys.argv[1:])
+    search_term = input("What do you want to see?\n> ")
+else:
+    search_term = '_'.join(sys.argv[1:])
 os.mkdir(search_term)
 os.chdir(search_term)
 
@@ -15,12 +15,12 @@ headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101
 r = requests.get("https://www.google.com/search?tbm=isch&q={}".format(search_term),headers=headers)
 s = BeautifulSoup(r.text,'html.parser')
 
-images = s.select('img')
+images = s.findAll('img',attrs={'data-src':re.compile(r'[\w+]')})
 
 try:
     for i in images[:5]:
         input("Press 'enter' to see images...\n")
-        im = requests.get(i.get('src'))
+        im = requests.get(i.get('data-src'))
         path = 'img{}.jpg'.format(images.index(i))
         with open(path,"wb+") as f:
             f.write(im.content)
