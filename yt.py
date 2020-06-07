c7b6307641b9540188c05514977d5148726e0a42
Suhas Kowligi <kowligi1998@gmail.com>
2020-06-07 18:32:59 +0530

    Update yt.py

--- yt.py
+++ yt.py
@@ -1,61 +1,61 @@
 #!/usr/bin/python3
 import re,os,sys
-from subprocess import PIPE,Popen
+from subprocess import PIPE,run,Popen
 import requests
 from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 
 if len(sys.argv[1:])==0:
-    exit("Usage : yt.py [search_term]")
+    search_term = ' '.join(input("Enter the search term\n").split())
 else:
     search_term = ' '.join(sys.argv[1:])
 
-headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0'}
+headers = {'User-Agent':UserAgent().random}
 
 try:
-    r = requests.get("http://youtube.com/results?search_query=" + '+'.join(search_term),headers=headers)
+    r = requests.get("http://youtube.com/results?search_query=" + '+'.join(search_term.split()))
 except requests.HTTPError as err:
     print(err)
     exit()
 
 print("Searching....")
 s = BeautifulSoup(r.text, 'html.parser')
-l = s.select('div .yt-lockup-content')
+l = s.findAll('a',{'class':'yt-uix-tile-link yt-ui-ellipsis yt-ui-ellipsis-2 yt-uix-sessionlink spf-link','rel':'spf-prefetch'})
 
 urls = []
 titles = []
-views = []
+durations = []
 uploaded = []
+views = []
 uploaders = []
-durations =[]
 
 for i in l:
     try:
-        meta = i.findAll('li')[:2]
+        urls.append('http://youtube.com'+i.get('href'))
+        titles.append(i.text)
+        durations.append(re.findall('\w+:\w+',i.parent.text)[0])
+        meta = i.parent.parent.find('ul',{'class':'yt-lockup-meta-info'}).findAll('li')
         uploaded.append(meta[0].text)
         views.append(meta[1].text)
-        durations.append(i.find('span',attrs={'class':'accessible-description'}).text)
-        urls.append('http://youtube.com'+i.find('a').get('href'))
-        titles.append(i.find('a').get('title'))
-        uploader = i.findAll('a')[1].get('href')
-        uploaders.append(re.findall('/user/(\w+)',uploader)[0])
+        uploaders.append(i.parent.parent.find('div',{'class':"yt-lockup-byline"}).text)
     except:
         pass
 
 for i,t,up,d,v,ur,us in zip(range(5),titles,uploaded,durations,views,urls,uploaders):
     try:
-        p=requests.get(ur,headers=headers)
-        opinion=re.findall(r'(\d+(,\d+)*)? other people',p.text)
-        likes = opinion[0][0]
-        dislikes=opinion[2][0]
-        print(str(i)+' : '+t+'\nYouTube URL : '+ur+'\nUPLOADED : '+up+'\n'+d.upper().replace(' - ','')+'\nVIEWS : '+v+'\nLIKES : '+likes+'\nDISLIKES : '+dislikes+'\nUPLOADER : '+us+'\n')
-    except requests.exceptions.ConnectionError:
-        pass
-    except IndexError:
+        p = requests.get(ur)
+        q = BeautifulSoup(p.text,'html.parser')
+        likes = q.find('button',{'title':'I like this'}).text
+        dislikes = q.find('button',{'title':'I dislike this'}).text
+        print('<<< '+str(i)+' >>> : '+t+'\nYouTube URL\t:\t'+ur+'\nUPLOADED\t:\t'+up+'\nDURATION\t:\t'+d+'\nVIEWS\t\t:\t'+v+'\nLIKES\t\t:\t'+likes+'\nDISLIKES\t:\t'+dislikes+'\nUPLOADER\t:\t'+us+'\n')
+    except:
         pass
 
 
 while True:
     ch=input("Enter the number corresponding to the link (type 'exit' to quit)- \n")
+    if ch=='exit':
+        exit('Bye')
     for i,j in enumerate(urls):
         if ch==str(i):
             print(j)
@@ -64,15 +64,13 @@ while True:
                 mp3=input("Do you want to download the audio (type 'a') or video (type 'v')?\n")
                 if mp3 == 'v':
                     print("Downloading video...")
-                    run("youtube-dl "+j,shell=True)
+                    run(["youtube-dl",j],shell=True)
                 else:
                     print("Downloading audio...")
-                    run("youtube-dl --extract-audio --audio-format mp3 "+j,shell=True)
+                    run(["youtube-dl", "--extract-audio", "--audio-format","mp3",j])
             elif dorv == 'v':
                 print("Streaming...")
                 try:
-                    Popen(['vlc',j],stderr=PIPE,stdout=PIPE,close_fds=True)
+                    Popen(['vlc',j],stderr=PIPE,stdout=PIPE)
                 except FileNotFoundError:
-                    Popen([input("Enter your default media player...\nEx: 'mpv','totem' : "),j],stderr=PIPE,stdout=PIPE,close_fds=True)
-        elif ch=='exit':
-            exit()
+                    Popen([input("Enter your default media player...\nEx: 'mpv','totem','vlc' : "),j],stderr=PIPE,stdout=PIPE,close_fds=True)
